757bb68f77d9cd05b63d193a357017c3537721aa
Veysel Dogan <veysel.dogan@ogr.sakarya.edu.com.tr>
2019-12-04 12:07:33 +0300

    Admin panelini olusturdum ve veritabanindan onlara ait verileri tablolara cektim

---
+++ ProjeCV/Controllers/EgitimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeCV.Models.Entity;
+using ProjeCV.Models.Sinif;
+namespace ProjeCV.Controllers
+{
+    public class eGITIMController : Controller
+    {
+        // GET: Deneyimler
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger3 = db.TBLEDUCATION.ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ ProjeCV/Controllers/HobilerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeCV.Models.Entity;
+using ProjeCV.Models.Sinif;
+namespace ProjeCV.Controllers
+{
+    public class HobilerController : Controller
+    {
+        // GET: Hobiler
+        DbCvEntities db = new DbCvEntities();
+
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger5 = db.TBLINTEREST.ToList();
+            return View(cs);
+        }
+    }
+}
